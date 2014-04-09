e8173d14f760508728e2f2717ecd32f1129ec8fb
Dan <dan.pantry@hotmail.com>
2014-04-09 20:08:09 +0100

    implemented members for PokemonItem

--- NXT/NXT.DAL/Entities/Pokemons/PokemonItem.cs
+++ NXT/NXT.DAL/Entities/Pokemons/PokemonItem.cs
@@ -1,11 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace NXT.DAL.Entities.Pokemons
+﻿namespace NXT.DAL.Entities.Pokemons
 {
     public class PokemonItem
     {
+        // Primary key = Pokemon + version + Item
+        public Pokemon Pokemon { get; set; }
+        public GameVersion Version { get; set; }
+        public Item Item { get; set; }
+
+        public int Rarity { get; set; }
     }
 }
