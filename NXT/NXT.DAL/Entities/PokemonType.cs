637c62c5ce6d8810456632e7c1e7ccca9513ad0e
Dan <dan.pantry@hotmail.com>
2014-04-09 19:05:40 +0100

    Woops, access level inconsistency

--- NXT/NXT.DAL/Entities/PokemonType.cs
+++ NXT/NXT.DAL/Entities/PokemonType.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace NXT.DAL.Entities
+﻿namespace NXT.DAL.Entities
 {
-    class PokemonType
+    public class PokemonType
     {
     }
 }
