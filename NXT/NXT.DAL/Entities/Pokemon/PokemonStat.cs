363c487ca87006af5594fb22c306d09b9c8a34bf
Dan <dan.pantry@hotmail.com>
2014-04-09 19:53:02 +0100

    Implemented PokemonStat members

--- NXT/NXT.DAL/Entities/Pokemon/PokemonStat.cs
+++ NXT/NXT.DAL/Entities/Pokemon/PokemonStat.cs
@@ -7,5 +7,9 @@ namespace NXT.DAL.Entities.Pokemons
 {
     public class PokemonStat
     {
+        public int Effort { get; set; }
+        public int Base { get; set; }
+        public Pokemon Pokemon { get; set; }
+        public Stat Stat { get; set; }
     }
 }
