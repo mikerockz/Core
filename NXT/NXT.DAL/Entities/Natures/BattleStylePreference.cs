9324897c342f358ae21346a8cc2e4f7876c65d58
Dan <dan.pantry@hotmail.com>
2014-04-13 18:52:28 +0100

    Moving Nature stuff to Natures namespace

--- NXT/NXT.DAL/Entities/Natures/BattleStylePreference.cs
+++ NXT/NXT.DAL/Entities/Natures/BattleStylePreference.cs
@@ -1,6 +1,11 @@
-﻿namespace NXT.DAL.Entities.Natures
+﻿using NXT.DAL.Entities.Moves;
+namespace NXT.DAL.Entities.Natures
 {
     public class BattleStylePreference
     {
+        public Nature Nature { get; set; }
+        public MoveBattleStyle MovementBattleStyle { get; set; }
+        public int LowHpPreference { get; set; }
+        public int HighHpPreference { get; set; }
     }
 }
--- NXT/NXT.DAL/Entities/Natures/Nature.cs
+++ NXT/NXT.DAL/Entities/Natures/Nature.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace NXT.DAL.Entities.Nature
+﻿namespace NXT.DAL.Entities.Natures
 {
     public class Nature
     {
