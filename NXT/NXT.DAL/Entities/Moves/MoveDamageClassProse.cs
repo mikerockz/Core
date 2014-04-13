1d990cef5e138a9b292c4686e1e4699b9f73f9f2
Dan <dan.pantry@hotmail.com>
2014-04-13 19:24:53 +0100

    adding language to move damage class prose

--- NXT/NXT.DAL/Entities/Moves/MoveDamageClassProse.cs
+++ NXT/NXT.DAL/Entities/Moves/MoveDamageClassProse.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
+﻿using NXT.DAL.Entities.Languages;
 namespace NXT.DAL.Entities.Moves
 {
     public class MoveDamageClassProse
     {
         public MoveDamageClass MoveDamageClass { get; set; }
-        public int LocalLanguageId { get; set; }
+        public Language Language { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
     }
