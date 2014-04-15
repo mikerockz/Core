e8b1df8795dccbe0eeeb7945f2c093ed0bf4030f
Dan <dan.pantry@hotmail.com>
2014-04-15 15:47:59 +0100

    Adding initial Region map

---
+++ NXT/NXT.DAL.EF/EntityMaps/RegionMap.cs
@@ -0,0 +1,14 @@
+﻿using NXT.DAL.Entities.Regions;
+using System.Data.Entity.ModelConfiguration;
+
+namespace NXT.DAL.EF.EntityMaps
+{
+    public class RegionMap : EntityTypeConfiguration<Region>
+    {
+        public RegionMap()
+        {
+            HasKey(x => x.Id);
+            Property(x => x.Identifier);
+        }
+    }
+}
