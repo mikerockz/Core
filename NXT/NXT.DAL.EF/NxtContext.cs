d0fd5d9c474b66354edd8b2d8d1b4353b7e1d0b4
Dan <dan.pantry@hotmail.com>
2014-04-15 14:05:24 +0100

    Use the correct entity framework version - and allow the context to register EntityTypeConfigurations from the assembly it is declared in

---
+++ NXT/NXT.DAL.EF/NxtContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+namespace NXT.DAL.EF
+{
+    public class NxtContext : DbContext
+    {
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            // Configure EF mappings here
+            // or create an EntityTypeConfiguration<T> class
+            var thisAssembly = typeof(NxtContext).Assembly;
+            modelBuilder.Configurations.AddFromAssembly(thisAssembly);
+        }
+    }
+}
